4aea48e9dc51b5ff2bd7df3d574e9bd03a422d48
Dr. <106764528+RunnerOnFoot@users.noreply.github.com>
2024-04-03 16:03:27 +0330

    use "Other Useful String Methods"

---
+++ chapter_2/ex_2-3.js
@@ -0,0 +1,9 @@
+let inputText = "Here is your mail Mr. P.";
+let mail = "mail";
+
+
+console.log(inputText.toLowerCase());
+console.log(inputText.includes(mail));
+console.log(inputText.padStart(30, "f"));
+console.log(inputText.repeat(0));
+console.log(inputText.repeat(3));
