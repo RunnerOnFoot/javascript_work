b2250e58cb805293c106dd5559782dc180805c09
Dr. <106764528+RunnerOnFoot@users.noreply.github.com>
2024-04-10 16:20:46 +0330

    triple equals operator

---
+++ chapter_2/booleans/comparison_operators/1.js
@@ -0,0 +1,20 @@
+console.log(5 === 5);
+// true
+
+console.log(6 === 7);
+// false
+
+console.log(2 + 2 === 4);
+// true
+
+console.log("hello" === "goodbye");
+// false
+
+console.log("hello" === "hell" + "o");
+// true
+
+console.log(false === false);
+// true
+
+console.log(true === false);
+// false
