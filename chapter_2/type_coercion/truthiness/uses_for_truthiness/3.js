9b4ad76a51e3451699c5c4539e9530652ec4b9e2
Dr. <106764528+RunnerOnFoot@users.noreply.github.com>
2024-04-15 13:50:30 +0330

    Use && for Truthiness

---
+++ chapter_2/type_coercion/truthiness/uses_for_truthiness/3.js
@@ -0,0 +1,3 @@
+let score = 0;
+score && alert(`Your score is ${score}!`);
+// 0
---
+++ chapter_2/type_coercion/truthiness/uses_for_truthiness/4.js
@@ -0,0 +1,5 @@
+let score = 0;
+++score;
+// 1
+score && alert(`Your score is ${score}!`)
+// undefined -> and shows the alert!
