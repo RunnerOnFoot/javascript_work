46acd4db2d5be654d2ec442b6bd0c88335634198
Dr. <106764528+RunnerOnFoot@users.noreply.github.com>
2024-04-19 19:17:36 +0330

    Set an "X" in the top-right corner.

--- chapter_3/arrays/arrays_of_arrays/2.js
+++ chapter_3/arrays/arrays_of_arrays/2.js
@@ -6,3 +6,14 @@ let ticTacToe = [
 
 ticTacToe;
 // (3) [Array(3), Array(3), Array(3)]
+
+ticTacToe[0][2] = "X";
+// "X"
+
+ticTacToe;
+// (3) [Array(3), Array(3), Array(3)]
+// 0: (3) ['', '', 'X']
+// 1: (3) ['', '', '']
+// 2: (3) ['', '', '']
+// length: 3
+// [[Prototype]]: Array(0)
