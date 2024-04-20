d05224a433befd10c6be481292dddaec582094d9
Dr. <106764528+RunnerOnFoot@users.noreply.github.com>
2024-04-20 17:51:45 +0330

    Adding an element to the beginning of the array with unshift method.

--- chapter_3/arrays/array_methods/adding_an_element_to_an_array/1.js
+++ chapter_3/arrays/array_methods/adding_an_element_to_an_array/1.js
@@ -9,3 +9,13 @@ languages.push("Rust");
 // 4
 languages;
 // (4) ['Python', 'Haskell', 'JavaScript', 'Rust']
+
+
+languages.unshift("Erlang");
+// 5
+languages.unshift("C");
+// 6
+languages.unshift("Fortran");
+// 7
+languages;
+// (7) ['Fortran', 'C', 'Erlang', 'Python', 'Haskell', 'JavaScript', 'Rust']
