04378d46fda5e4d91b1027f151d26bdddb748b19
Dr. <106764528+RunnerOnFoot@users.noreply.github.com>
2024-04-20 17:46:50 +0330

    Adding an element to the end of an array with push method.

---
+++ chapter_3/arrays/array_methods/adding_an_element_to_an_array/1.js
@@ -0,0 +1,11 @@
+let languages = [];
+languages.push("Python");
+// 1
+languages.push("Haskell");
+// 2
+languages.push("JavaScript");
+// 3
+languages.push("Rust");
+// 4
+languages;
+// (4) ['Python', 'Haskell', 'JavaScript', 'Rust']
