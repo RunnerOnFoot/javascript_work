58d6ce9fd766cec99e9c0b37da739ee370dca0a6
Dr. <106764528+RunnerOnFoot@users.noreply.github.com>
2024-04-21 23:18:44 +0330

    Removing an element from an array with the pop method.

---
+++ chapter_3/arrays/array_methods/removing_elements_from_an_array/1.js
@@ -0,0 +1,7 @@
+let languages = ["Erlang", "C", "Fortran",
+    "Python", "Haskell", "JavaScript", "Rust"];
+
+languages.pop();
+// 'Rust'
+languages;
+// (6) ['Fortran', 'C', 'Erlang', 'Python', 'Haskell', 'JavaScript']
