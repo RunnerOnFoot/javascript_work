40e3a335d5aa43d433d84f479e1147de129602a7
Dr. <106764528+RunnerOnFoot@users.noreply.github.com>
2024-04-22 16:22:27 +0330

    Delete another element form the languages array and use it in a message.

--- chapter_3/arrays/array_methods/removing_elements_from_an_array/1.js
+++ chapter_3/arrays/array_methods/removing_elements_from_an_array/1.js
@@ -5,3 +5,10 @@ languages.pop();
 // 'Rust'
 languages;
 // (6) ['Fortran', 'C', 'Erlang', 'Python', 'Haskell', 'JavaScript']
+
+let bestLanguage = languages.pop();
+let message = `My favorite language is ${bestLanguage}.`;
+message;
+// 'My favorite language is JavaScript.'
+languages;
+// (5) ['Fortran', 'C', 'Erlang', 'Python', 'Haskell']
