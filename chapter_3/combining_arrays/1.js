c8f74b05f4492be17a9990265fca597f5212ec76
Dr. <106764528+RunnerOnFoot@users.noreply.github.com>
2024-04-23 23:58:28 +0330

    Understanding concat method (short for concatenate).

---
+++ chapter_3/combining_arrays/1.js
@@ -0,0 +1,5 @@
+let fish = ["Salmon", "Cod", "Trout"];
+let mammals = ["Sheep", "Cat", "Tiger"];
+let animals = fish.concat(mammals);
+animals;
+// (6) ['Salmon', 'Cod', 'Trout', 'Sheep', 'Cat', 'Tiger']
