2bc5f327df61a0b45c04f2e1dfc999271095c4ce
Dr. <106764528+RunnerOnFoot@users.noreply.github.com>
2024-04-24 12:52:01 +0330

    Combine three or more arrays with concat method.

---
+++ chapter_3/combining_arrays/2.js
@@ -0,0 +1,6 @@
+let originals = ["Hope", "Empire", "Jedi"];
+let prequels = ["Phantom", "Clones", "Sith"];
+let sequels = ["Awakens", "Last", "Rise"];
+let starWars = prequels.concat(originals, sequels);
+starWars;
+// (9) ['Phantom', 'Clones', 'Sith', 'Hope', 'Empire', 'Jedi', 'Awakens', 'Last', 'Rise']
