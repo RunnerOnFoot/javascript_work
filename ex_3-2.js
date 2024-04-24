99f84d927f21d835415dee327e91ce6072756d50
Dr. <106764528+RunnerOnFoot@users.noreply.github.com>
2024-04-24 12:38:45 +0330

    Exercise 3-2

---
+++ ex_3-2.js
@@ -0,0 +1,11 @@
+let rainbow = [];
+
+rainbow.push("Green");
+rainbow.unshift("Yellow");
+rainbow.unshift("Orange");
+rainbow.unshift("Red");
+rainbow.push("Blue");
+rainbow.push("Indigo");
+rainbow.push("Violet");
+console.log(rainbow);
+// (7) ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
