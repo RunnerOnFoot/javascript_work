2097f5b0b2f037a2fb9c98f3a39753290bb86cfa
Dr. P <106764528+RunnerOnFoot@users.noreply.github.com>
2024-05-11 23:58:43 +0330

    Create an object

---
+++ chapter_3/objects/1.js
@@ -0,0 +1,7 @@
+let casablanca = {
+    "title": "Casablanca",
+    "released": 1942,
+    "director": "Michael Curtiz"
+};
+casablanca;
+// {title: 'Casablanca', released: 1942, director: 'Michael Curtiz'}
