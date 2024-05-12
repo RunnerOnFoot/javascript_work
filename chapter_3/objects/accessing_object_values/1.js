2b9acd2adc10e9e3b929d473fc735d73b8890453
Dr. P <106764528+RunnerOnFoot@users.noreply.github.com>
2024-05-12 22:51:53 +0330

    Accessing Object Values

---
+++ chapter_3/objects/accessing_object_values/1.js
@@ -0,0 +1,12 @@
+let casablanca = {
+    "title": "Casablanca",
+    "released": 1942,
+    "director": "Michael Curtiz"
+};
+
+let obj = { key1: 1, key_2: 2, "key 3": 3, "key#4": 4 };
+
+obj["key 3"];
+// 3
+casablanca["title"];
+// 'Casablanca'
